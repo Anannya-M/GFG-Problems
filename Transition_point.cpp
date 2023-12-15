ce636906188170ca4a87c43e2f8d1a26b9127850
Anannya <anannyamanojawas@gmail.com>
2023-12-15 23:28:54 +0530

    Determining Transition point

---
+++ Transition_point.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:    
+    
+    int transitionPoint(int arr[], int n) {
+        // code here
+        int low=0;
+        int high= n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==0)
+                low= mid+1;
+            else if(arr[mid]==1){
+                if(mid==0 || (mid>0 && arr[mid-1]==0))
+                    return mid;
+                high= mid-1;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int transitionPoint(int arr[], int n);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.transitionPoint(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
