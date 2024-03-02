f813407bd4dbec002194bff54683dc932524c316
Anannya Manojawas <85965261+Anannya-M@users.noreply.github.com>
2024-03-02 23:27:02 +0530

    Create First_element_to_occur_k_times.cpp

---
+++ First_element_to_occur_k_times.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        unordered_map<int, int> freqMap;
+
+    for (int i = 0; i < n; i++) {
+        freqMap[a[i]]++;
+        if (freqMap[a[i]] == k) {
+            return a[i];
+        }
+    }
+
+    return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(n, k, a)<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
